fbe37f2183a5a9b8b6253fb2599a168620dcf32a
sainikhilvenkata <55308570+sainikhilvenkata@users.noreply.github.com>
2022-06-08 07:30:04 +0530

    Time: 84 ms (19.35%), Space: 14 MB (63.83%) - LeetHub

--- 415-add-strings/415-add-strings.py
+++ 415-add-strings/415-add-strings.py
@@ -1,4 +1,27 @@
 class Solution:
     def addStrings(self, num1: str, num2: str) -> str:
-        return str(int(num1)+int(num2))
+        m,n=len(num1)-1,len(num2)-1
+        carry=0
+        out=''
+        while n>-1 or m>-1:
+            i,j=0,0
+            if m>-1:
+                i=ord(num1[m])-ord('0')
+                m-=1
+            if n>-1:
+                j=ord(num2[n])-ord('0')
+                n-=1
+           
+            tmp=i+j+carry
+            if tmp>9:
+                carry=1
+            else:
+                carry=0
+            tmp=str(tmp)
+            out=tmp[-1]+out
+        if carry==0:
+            return out
+        else:
+            return "1"+out
+            
         
